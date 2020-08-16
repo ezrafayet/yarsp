f5e6905f600eaabe91109024cbfa6766fbdd3803
adzaria <adzaria@protonmail.com>
2020-08-16 10:37:31 +0300

    added jest and unit tests structure

---
+++ src/components/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+import {App} from "./App";
+
+describe("<App />", () => {
+  test("Empty test", async () => {
+  
+  });
+});
