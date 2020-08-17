ed86ae07702e8dac11f2f4eb96289eb6d71b426c
adzaria <adzaria@protonmail.com>
2020-08-17 14:23:27 +0300

    added a temporary form handler

---
+++ src/utils/AbstractFormHandler.ts
@@ -0,0 +1,25 @@
+
+export {AbstractFormHandler};
+
+/**
+ * This form handler is meant to work with useReducer only
+ */
+class AbstractFormHandler {
+  
+  constructor() {
+  
+  }
+  
+  update () {
+  
+  }
+  
+  reset () {
+  
+  }
+  
+  send() {
+  
+  }
+  
+}
